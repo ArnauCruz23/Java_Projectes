18443dc8ad9188a968d207d2811a2285a7bd1a73
Arnau Cruz <cruzcambra.arnau@gmail.com>
2020-10-13 20:34:31 +0200

    Primer Commit

---
+++ src/projectesjava/Triangulo.java
@@ -0,0 +1,57 @@
+
+package projectesjava;
+
+public class Triangulo {
+    
+    //propiedades
+    private double base;
+    private double altura;
+    
+    public Triangulo(double a, double b) {
+        this.altura = a;
+        this.base = b;
+    }
+    
+    //Getters y Setters
+    public double getBase() {
+        return base;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+    
+    //To String
+    @Override
+    public String toString() {
+        return "Triangulo{" + "base= " + base + ", altura= " + altura + '}';
+    }
+    
+    //Merodos
+    public double Costat(){
+        double mediabase = this.base/2;
+        return Math.sqrt((mediabase*mediabase)+(altura*altura));
+    }
+    
+    public double area(){
+        return((this.base*this.altura)/2); 
+    }
+    
+    public double perimetre(){
+        return (this.Costat()*2) + this.base;
+    }
+    
+    public static void main(String[] args){
+        Triangulo tri = new Triangulo(17, 19);
+        System.out.println(tri);
+        System.out.print("area = "+tri.area() + ", perimtre = "+tri.perimetre());
+    }  
+}
