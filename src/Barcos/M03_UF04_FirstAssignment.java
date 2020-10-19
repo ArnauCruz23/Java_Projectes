853bafbd956700d638ed123db0f23a6d0b382b5d
Arnau Cruz <cruzcambra.arnau@gmail.com>
2020-10-19 15:28:56 +0200

    Un commit

--- src/Barcos/M03_UF04_FirstAssignment.java
+++ src/Barcos/M03_UF04_FirstAssignment.java
@@ -12,14 +12,15 @@ public class M03_UF04_FirstAssignment {
      * 
      * Crea una nueva subclase o más de BarcoDeMercancias llamada Carguero
      * 
-     * 
      */
     public static void main(String[] args) {
         
         Barco barcoMercante = new Carguero(1000, new Motor(200), 1, 2, 1, 2000);
+        
         barcoMercante.arranca();
         barcoMercante.atraca();
         barcoMercante.comportamiento();
+        
         
         Barco bismark = new AcorazadoBismark(1000, new Motor(3000), 1, 2, 3, 3, 5);
         
