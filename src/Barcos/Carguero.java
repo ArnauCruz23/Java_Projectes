d40b3f304f83bee547387eea05d071b43b10f1e9
Arnau Cruz <cruzcambra.arnau@gmail.com>
2020-10-19 13:47:47 +0200

    Netejant Codi

--- src/Barcos/Carguero.java
+++ src/Barcos/Carguero.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Barcos;
 
 public class Carguero extends BarcoDeMercancias{
-    private double kilosDeMercancia;
+    private double pesoMercancia;
 
-    public Carguero(double kilosDeMercancia, Motor motor, double eslora, double manga, int nudos, int peso) {
+    public Carguero(double pesoMercancia, Motor motor, double eslora, double manga, int nudos, int peso) {
         super(motor,eslora, manga, nudos, peso);
-        this.kilosDeMercancia = kilosDeMercancia;
+        this.pesoMercancia = pesoMercancia;
     }
 }
--- src/Barcos/M03_UF04_FirstAssignment.java
+++ src/Barcos/M03_UF04_FirstAssignment.java
@@ -1,15 +1,8 @@
-/*
- * FIRST ASSIGNMENT
- */
 package Barcos;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author pep
- */
 public class M03_UF04_FirstAssignment {
 
     /**
@@ -22,46 +15,12 @@ public class M03_UF04_FirstAssignment {
      * 
      */
     public static void main(String[] args) {
-        // Prueba to código aquí
         
         Barco barcoMercante = new Carguero(1000, new Motor(200), 1, 2, 1, 2000);
         barcoMercante.arranca();
         barcoMercante.atraca();
         barcoMercante.comportamiento();
         
-        
-        /*Barco barco1 = new Yate(true, 4, 6, new Motor(1000), 20.0, 12.5, 23, 7);
-        barco1.arranca();
-        
-        Barco barco2 = new PortaAviones(35, new Motor(6000), 250, 75, 46, 60_000, 35);
-        barco2.arranca();
-        
-        System.out.println(barco1 instanceof PortaAviones);*/
-        
-        
-//        CuentaCorriente cc1 = new CuentaCorriente("Pedro", 1_000.0);
-//        System.out.println(cc1);
-//        try {
-//            cc1.ingresa(123.09);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        System.out.println(cc1);
-//        try {
-//            cc1.abona(98.87);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        System.out.println(cc1);
-//
-//        Cuadrado c1 = null;
-//        try {
-//            c1 = new Cuadrado(9);
-//            System.out.println(c1.area());
-//            System.out.println(c1.perimetro());
-//        } catch (Exception ex) {
-//            ex.printStackTrace();
-//        }
 
     }
 
